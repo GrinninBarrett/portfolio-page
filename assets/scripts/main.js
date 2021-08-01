f08d65c405601f01cf7809cc049bc41baf25a1c7
Tucker Barrett <tuckerbarrett@gmail.com>
2021-07-31 22:19:36 -0400

    Improves mobile nav list

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,9 +10,23 @@ hamburgerIcon.addEventListener("click", () => {
     hamburgerIcon.classList.toggle("is-active");
     if (!menuOpen) {
         navLinksEl.style.display = "block";
+        navLinksEl.style.right = "0";
+        hamburgerIcon.setAttribute("aria-expanded", "true");
         menuOpen = true;
     } else {
         navLinksEl.style.display = "none";
+        navLinksEl.style.right = "-1000";
+        hamburgerIcon.setAttribute("aria-expanded", "false");
         menuOpen = false;
+    }
+})
+
+
+// Ensure normal nav links display if going from narrow window to wider window on desktop
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 768) {
+        navLinksEl.style.display = "block";
+    } else {
+        navLinksEl.style.display = "none";
     }
 })
